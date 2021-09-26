24bbd8535113e7f89e3bc54d7c570e3b00341820
jeanc <jeancolexxx@gmail.com>
2021-09-26 06:36:57 +0100

    Project Start

---
+++ app/src/main/java/com/codingC0l3/colepad/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.codingC0l3.colepad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.codingC0l3.colepad.ui.theme.ColePadTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ColePadTheme {
+
+            }
+        }
+    }
+}
